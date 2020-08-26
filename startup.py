1c8e95abae192086d50c111834ec297a9db99610
suneelthakur1980 <suneelthakur@gmail.com>
2020-08-26 11:49:18 +0530

    Update startup.py

--- startup.py
+++ startup.py
@@ -6,6 +6,6 @@ file through Gunicorn; the result is "Attempted relative import in non-package".
 
 The solution is to provide a simple alternate startup file, like this present
 startup.py, that just imports the app object. You can then just specify
-startup:app in the Gunicorn command.
+startup:app in the Gunicorn command. test
 """
 from hello_app.webapp import app
